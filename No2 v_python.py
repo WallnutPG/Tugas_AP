914beb3c3b9045835f2b5fde6310bee866817c10
WallnutPG <113717223+WallnutPG@users.noreply.github.com>
2023-03-14 09:24:53 +0700

    Create No2 v_python.py

---
+++ No2 v_python.py
@@ -0,0 +1,9 @@
+# program menghitung suku ke-n dari deret aritmatika
+a = 1
+b = 2
+n = 5
+
+# rumus untuk mencari suku ke-n
+nth_term = a + (n-1)*b
+
+print("Suku ke-", n, "dari deret aritmatika dengan beda", b, "dan suku pertama", a, "adalah:", nth_term)
