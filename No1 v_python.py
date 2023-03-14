70bf5c50e790dc2b322c47f1c9997d7bf26059b6
WallnutPG <113717223+WallnutPG@users.noreply.github.com>
2023-03-14 09:23:45 +0700

    Create No1 v_python.py

---
+++ No1 v_python.py
@@ -0,0 +1,16 @@
+# program pertukaran nilai dua variabel
+a = 10
+b = 5
+
+print("Sebelum pertukaran nilai:")
+print("a =", a)
+print("b =", b)
+
+# melakukan pertukaran nilai
+temp = a
+a = b
+b = temp
+
+print("\nSetelah pertukaran nilai:")
+print("a =", a)
+print("b =", b)
